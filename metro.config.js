a1b1060433206eb6293a65f9ec9d5d88e9dd57b0
JIMIN PARK <103014298+keepinblazing@users.noreply.github.com>
2023-06-15 00:06:18 +0900

    fix: 빌드 오류 해결중

--- metro.config.js
+++ metro.config.js
@@ -5,9 +5,6 @@ module.exports = (async () => {
     resolver: {sourceExts, assetExts},
   } = await getDefaultConfig();
   return {
-    transformer: {
-      babelTransformerPath: require.resolve('react-native-svg-transformer'),
-    },
     resolver: {
       assetExts: assetExts.filter((ext) => ext !== 'svg'),
       sourceExts: [...sourceExts, 'svg'],
