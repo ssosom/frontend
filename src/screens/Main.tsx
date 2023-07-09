0292bc1bbd4a1695d3befea13799dbc779664c14
JIMIN PARK <103014298+keepinblazing@users.noreply.github.com>
2023-07-09 17:19:46 +0900

    feat: ios 녹음, 재생, 확인 성공 android 디버깅중

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,40 +1,58 @@
 import React, {useState} from 'react';
 import {Text, TouchableOpacity, View} from 'react-native';
 import MainLayout from '../components/Layout/MainLayout';
-import AudioRecorderPlayer, {AVEncoderAudioQualityIOSType, AVEncodingOption, AudioEncoderAndroidType, AudioSet, AudioSourceAndroidType} from 'react-native-audio-recorder-player';
+import AudioRecorderPlayer from 'react-native-audio-recorder-player';
+
+const audioRecorderPlayer = new AudioRecorderPlayer();
 
 const MainScreen = () => {
-  const audioRecorderPlayer = new AudioRecorderPlayer();
-  const [recoding, setRecoding] = useState<boolean>(false);
-  const [playerDuration, setPlayerDuration] = useState<any>('');
+  const [recordDuration, setRecordDuration] = useState({
+    recordSecs: 0,
+    recordTime: '00:00:00',
+  });
+  const [playerDuration, setPlayerDuration] = useState({
+    currentPositionSec: 0,
+    currentDurationSec: 0,
+    playTime: '00:00:00',
+    duration: '00:00:00',
+  });
+  const [recording, setRecording] = useState(false);
 
-  const onStartRecord = async () => {
-    const result = await audioRecorderPlayer.startRecorder();
-    audioRecorderPlayer.addRecordBackListener((e) => {
-      setRecoding(true);
+  // 녹음 시작
+  const handleStartRecord = async () => {
+    if (audioRecorderPlayer) {
+      setRecording(true);
       setPlayerDuration({
+        ...playerDuration,
+        currentPositionSec: 0,
+        currentDurationSec: 0,
+        playTime: '00:00:00',
+        duration: '00:00:00',
+      });
+      await audioRecorderPlayer.startRecorder();
+    }
+    audioRecorderPlayer.addRecordBackListener((e) => {
+      setRecordDuration({
+        ...recordDuration,
         recordSecs: e.currentPosition,
         recordTime: audioRecorderPlayer.mmssss(Math.floor(e.currentPosition)),
       });
-      return;
     });
-    console.log(result);
   };
 
-  const onStopRecord = async () => {
-    const result = await audioRecorderPlayer.stopRecorder();
+  // 녹음 중지
+  const handleStopRecord = async () => {
+    if (audioRecorderPlayer) {
+      setRecording(false);
+      await audioRecorderPlayer.stopRecorder();
+    }
     audioRecorderPlayer.removeRecordBackListener();
-    setRecoding(false);
-    // setState({
-    //   recordSecs: 0,
-    // });
-    console.log(result);
+    setRecordDuration({...recordDuration, recordSecs: 0});
   };
 
-  const onStartPlay = async () => {
-    console.log('onStartPlay');
-    const msg = await audioRecorderPlayer.startPlayer();
-    console.log(msg);
+  // 음성 재생
+  const soundStart = async () => {
+    await audioRecorderPlayer.startPlayer();
     audioRecorderPlayer.addPlayBackListener((e) => {
       setPlayerDuration({
         currentPositionSec: e.currentPosition,
@@ -42,18 +60,7 @@ const MainScreen = () => {
         playTime: audioRecorderPlayer.mmssss(Math.floor(e.currentPosition)),
         duration: audioRecorderPlayer.mmssss(Math.floor(e.duration)),
       });
-      return;
     });
-  };
-
-  const onPausePlay = async () => {
-    await audioRecorderPlayer.pausePlayer();
-  };
-
-  const onStopPlay = async () => {
-    console.log('onStopPlay');
-    audioRecorderPlayer.stopPlayer();
-    audioRecorderPlayer.removePlayBackListener();
   };
 
   return (
@@ -61,9 +68,19 @@ const MainScreen = () => {
       <View className={`w-full h-5 flex justify-center items-end pr-3 bg-[#7F73DB]`}></View>
       <View className="w-full h-[90%] bg-white">
         <View className="w-full h-[60%]"></View>
-        <TouchableOpacity className="w-10 h-5 border" onPress={() => onStartRecord()}>
-          <Text>녹음</Text>
-        </TouchableOpacity>
+        <View className="flex flex-row justify-center items-center gap-5">
+          <TouchableOpacity className="w-10 h-5 border" onPress={() => handleStartRecord()}>
+            <Text>녹음</Text>
+          </TouchableOpacity>
+          <TouchableOpacity className="w-10 h-5 border" onPress={() => handleStopRecord()}>
+            <Text>중지</Text>
+          </TouchableOpacity>
+          <TouchableOpacity className="w-10 h-5 border" onPress={() => soundStart()}>
+            <Text>재생</Text>
+          </TouchableOpacity>
+          <Text>{recordDuration.recordSecs}</Text>
+          <Text>{recordDuration.recordTime}</Text>
+        </View>
       </View>
     </MainLayout>
   );
